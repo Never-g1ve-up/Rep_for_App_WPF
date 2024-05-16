a1cabfe33c1d5505ee7c359765e93799707b9bb2
Never-g1ve-up <123229060+Never-g1ve-up@users.noreply.github.com>
2024-05-16 11:24:39 +0300

    Создание папок + загрузка Prism

---
+++ src/App_for_bank/ViewModels/MainWindVW.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_for_bank.ViewModels
+{
+    internal class MainWindVW
+    {
+    }
+}
